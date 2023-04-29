81a72492a21d7579ef3d51a35be55e4377b616e1
DOMINATOR-XD <113828214+DOMINATOR-XD@users.noreply.github.com>
2023-04-29 19:04:53 +0530

    Update config.py

--- config /config.py
+++ config /config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 from dotenv import load_dotenv
 
-load_dotenv()
+()
 
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
@@ -24,31 +24,8 @@ SUDO_USER= list(
 
 # BOT TOKEN CONFIG VARS (get all vars detail from @botfather) 
 
-BOT_TOKEN0 = getenv("BOT_TOKEN0", None) 
-
-BOT_TOKEN1 = getenv("BOT_TOKEN1", None) 
-
-BOT_TOKEN2 = getenv("BOT_TOKEN2", None) 
-
-BOT_TOKEN3 = getenv("BOT_TOKEN3", None) 
-
-BOT_TOKEN4 = getenv("BOT_TOKEN4", None) 
-
-BOT_TOKEN5 = getenv("BOT_TOKEN5", None) 
-
-BOT_TOKEN6 = getenv("BOT_TOKEN6", None) 
-
-BOT_TOKEN7 = getenv("BOT_TOKEN7", None) 
-
-BOT_TOKEN8 = getenv("BOT_TOKEN8", None) 
-
-BOT_TOKEN9 = getenv("BOT_TOKEN9", None) 
-
-# EXTRA VARS
-
-ALIVE_PIC = getenv("ALIVE_PIC") 
-
-CMD_HNDLR = getenv("CMD_HNDLR") 
+n
+f("CMD_HNDLR") 
 
 LUND = list(
 
